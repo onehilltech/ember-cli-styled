9895eb94c9016fae667d23de6771f98cdfa0b434
James H. Hill <hilljh82@gmail.com>
2018-10-18 16:31:17 -0400

    Add generator for component styles

---
+++ blueprints/component-style/index.js
@@ -0,0 +1,45 @@
+/* eslint-env node */
+
+const { dasherize } = require ('ember-cli-string-utils');
+const { Blueprint } = require ('ember-cli-blueprint-helpers');
+
+module.exports = Blueprint.extend ({
+  description: 'Generates a material design style for an existing route',
+
+  availableOptions: Object.freeze ([
+    { name: 'path', type: String, default: ''},
+    { name: 'reset-namespace', type: Boolean }
+  ]),
+
+  fileMapTokens () {
+    return {
+      __stylepath__: function (options) {
+        let parts = options.locals.moduleName.split ('/');
+        let filename = parts[parts.length - 1];
+
+        parts[parts.length - 1] = '_' + filename;
+
+        return parts.join ('/');
+      }
+    };
+  },
+
+  locals (options) {
+    let moduleName = options.args[1];
+
+    if (options.resetNamespace)
+      moduleName = moduleName.split('/').pop();
+
+    let dasherizedModuleName = dasherize (moduleName);
+
+    return {
+      moduleName: dasherizedModuleName,
+      styleClassName: dasherizedModuleName.replace (/[/]/g, '-')
+    };
+  },
+
+  afterInstall () {
+    return this._locals (this.options)
+      .then (({moduleName, fileMap: { __root__ }}) => this.insertIntoFile (`${__root__}/styles/_components.scss`, `@import "components/${moduleName}";\n`))
+  }
+});
