44703bd612a822eb287fc4c057b5f9634805e1b4
James H. Hill <hilljh82@gmail.com>
2018-11-22 15:19:07 -0600

    fix: The addon was incompatible with ember-cli-mdc-sass

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const INCOMPATIBLE_PLUGINS = [
+  'ember-cli-mdc-sass'
+];
+
 module.exports = {
-  name: 'ember-cli-styled'
+  name: 'ember-cli-styled',
+
+  setupPreprocessorRegistry (type, registry) {
+    let registered = registry.registeredForType ('css');
+    let mdcIndex = registered.findIndex (plugin => INCOMPATIBLE_PLUGINS.includes (plugin.name));
+
+    if (mdcIndex !== -1) {
+      registry.remove ('css', 'broccoli-sass');
+      registry.remove ('css', 'ember-cli-sass');
+    }
+  }
 };
